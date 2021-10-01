04b1787031e9e9371161b2dedbef89321bd2b65d
Kostya <krot.kostya.krot@gmail.comm>
2021-10-02 01:01:34 +0300

    menu-burger

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,17 +3,7 @@ import './Content.css';
 function Content(){
     return(
         <div className="content-container">
-            <div className='content'>
-                <div className='main'>
-                    <p>Статья 1: sdcjvnjdfnvjfdn jdfnvkjldfnvljkvjk vjkldv lkdfn jvk jkdnv lkdnf jkdnfv ...</p>
-                    <p>Статья 2: sdcjvnjdfnvjfdn jdfnvkjldfnvljkvjk vjkldv lkdfn jvk jkdnv lkdnf jkdnfv ...</p>
-                </div>
-                {/*<div className='categories'>*/}
-                {/*    <p><input type="checkbox" name="Статьи" value="Статьи" checked />Статьи</p>*/}
-                {/*    <p><input type="checkbox" name="Фото" value="Фото"/>Фото</p>*/}
-                {/*    <p><input type="checkbox" name="Видео" value="Видео"/>Видео</p>*/}
-                {/*</div>*/}
-            </div>
+
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,53 @@
 import './Header.css';
+import {useState} from "react";
 
 function Header(){
+    const [activeChevron, setChevron] = useState(false);
+
+    function onClickChevron(){
+        setChevron(!activeChevron);
+    }
+
     return(
-        <div className='header'>
-            <div className="box">
-                <div className="container-1">
-                    <span className="icon"><i className="fa fa-search"></i></span>
-                    <input type="search" id="search" placeholder="Search..."/>
+        <div className='wrapper'>
+            <div className="header-container">
+                <div className="box">
+                    <div className="search-icon">
+                        <img src="https://www.svgrepo.com/show/18282/search.svg"
+                             alt="search"
+                             width='26px'
+                             height='26px'
+                        />
+                    </div>
+                    <input type="text" className='search-input' placeholder='Search...'/>
+                    <div onClick={ onClickChevron } className={`chevron${activeChevron?' chevronReverse':''}`}>
+                        <img src="https://www.svgrepo.com/show/183224/chevron.svg"
+                             alt="chevron"
+                             width='26px'
+                             height='26px'
+                        />
+                    </div>
                 </div>
+
+                {activeChevron &&
+                    <div className="categories">
+                        <div className="checkbox-item">
+                            <input type="checkbox" id='Статьи'/>
+                            <label htmlFor="Статьи">Статьи</label>
+                        </div>
+
+                        <div className="checkbox-item">
+                            <input type="checkbox" id='Фото'/>
+                            <label htmlFor="Фото">Фото</label>
+                        </div>
+
+                        <div className="checkbox-item">
+                            <input type="checkbox" id='Видео'/>
+                            <label htmlFor="Видео">Видео</label>
+                        </div>
+                    </div>
+                }
             </div>
-            <section className="group-burger">
-                <div className="burger"></div>
-                <div className="menu-burger">
-                    <div className="click-close"></div>
-                    <a href="#a1" className="menu-item">Все</a>
-                    <a href="#a2" className="menu-item">Статьи</a>
-                    <a href="#a3" className="menu-item">Фото</a>
-                    <a href="#a4" className="menu-item">Видео</a>
-                </div>
-            </section>
         </div>
     )
 }
