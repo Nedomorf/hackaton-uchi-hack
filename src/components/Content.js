48ce2ed9bb91e8f986b987b14b34ce6db243f966
Kostya <krot.kostya.krot@gmail.comm>
2021-10-01 17:23:34 +0300

    menu-burger

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,11 +8,11 @@ function Content(){
                     <p>Статья 1: sdcjvnjdfnvjfdn jdfnvkjldfnvljkvjk vjkldv lkdfn jvk jkdnv lkdnf jkdnfv ...</p>
                     <p>Статья 2: sdcjvnjdfnvjfdn jdfnvkjldfnvljkvjk vjkldv lkdfn jvk jkdnv lkdnf jkdnfv ...</p>
                 </div>
-                <div className='categories'>
-                    <p><input type="checkbox" name="Статьи" value="Статьи" checked />Статьи</p>
-                    <p><input type="checkbox" name="Фото" value="Фото"/>Фото</p>
-                    <p><input type="checkbox" name="Видео" value="Видео"/>Видео</p>
-                </div>
+                {/*<div className='categories'>*/}
+                {/*    <p><input type="checkbox" name="Статьи" value="Статьи" checked />Статьи</p>*/}
+                {/*    <p><input type="checkbox" name="Фото" value="Фото"/>Фото</p>*/}
+                {/*    <p><input type="checkbox" name="Видео" value="Видео"/>Видео</p>*/}
+                {/*</div>*/}
             </div>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,16 @@ function Header(){
                     <input type="search" id="search" placeholder="Search..."/>
                 </div>
             </div>
+            <section className="group-burger">
+                <div className="burger"></div>
+                <div className="menu-burger">
+                    <div className="click-close"></div>
+                    <a href="#a1" className="menu-item">Все</a>
+                    <a href="#a2" className="menu-item">Статьи</a>
+                    <a href="#a3" className="menu-item">Фото</a>
+                    <a href="#a4" className="menu-item">Видео</a>
+                </div>
+            </section>
         </div>
     )
 }
