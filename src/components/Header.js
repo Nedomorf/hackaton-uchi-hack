3cbb8f06bf87676046ab36ea24873d716bb24fd0
Kostya <krot.kostya.krot@gmail.comm>
2021-10-02 21:48:22 +0300

    on click items

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,11 @@ function Header() {
     const [activeChevron, setChevron] = useState(false);
     const [isArticlesChecked, setIsArticlesChecked] = useState(true);
     const [isPhotoChecked, setIsPhotoChecked] = useState(false);
-    const [isVideosChecked, setIsVideosChecked] = useState(true);
+    const [isVideosChecked, setIsVideosChecked] = useState(false);
+    const [isExcelChecked, setIsExcelChecked] = useState(false);
+    const [isPdfChecked, setIsPdfChecked] = useState(false);
+    const [isWordChecked, setIsWordChecked] = useState(false);
+    const [isPowerpointChecked, setIsPowerpointChecked] = useState(false);
     const [input, setInput] = useState('');
     const [transformedQuery, setTransformedQuery] = useState('');
 
@@ -19,6 +23,7 @@ function Header() {
     useEffect(() => {
         api.getPPT('соня+мармедадова')
     })
+
 
     function search() {
         if (!input) {
@@ -38,46 +43,6 @@ function Header() {
     }
 
     return (
-        // <div className='main'>
-        //     {/*<div className="header-container">*/}
-        //     <div className="search">
-        //         <div className="search-icon">
-        //             <img src="https://www.svgrepo.com/show/18282/search.svg"
-        //                  alt="search"
-        //                  width='26px'
-        //                  height='26px'
-        //             />
-        //         </div>
-        //         <input type="text" className='search-input' placeholder='Search...'/>
-        //         <div onClick={onClickChevron} className={`'chevron' ${!activeChevron && 'chevronReverse'}`}>
-        //             <img src="https://www.svgrepo.com/show/183224/chevron.svg"
-        //                  alt="chevron"
-        //                  width='26px'
-        //                  height='26px'
-        //             />
-        //         </div>
-        //     </div>
-        //
-        //     {activeChevron &&
-        //     <div className="categories">
-        //         <div className="checkbox-item">
-        //             <input type="checkbox" id="articles"/>
-        //             <label htmlFor="articles">Статьи</label>
-        //         </div>
-        //
-        //         <div className="checkbox-item">
-        //             <input type="checkbox" id="photo"/>
-        //             <label htmlFor="photo">Фото</label>
-        //         </div>
-        //
-        //         <div className="checkbox-item">
-        //             <input type="checkbox" id="videos"/>
-        //             <label htmlFor="videos">Видео</label>
-        //         </div>
-        //     </div>
-        //     }
-        //     {/*</div>*/}
-        // </div>
         <div className={styles.main}>
             <div className={styles.search}>
                 <div className={styles.searchIcon}>
@@ -102,11 +67,9 @@ function Header() {
             {/*// calculate height depends on items num ( (itemHeight * i) / 4 )*/}
             <div className={styles.categoriesContainer}>
                 <div className={`${styles.categories} ${activeChevron && styles.activeCategories}`}>
-                    <div className={styles.categoriesHeader}></div>
-                    <div className={`${styles.categoriesItem} ${isArticlesChecked && styles.checkedCategoriesItem}`}
-                         onClick={() => setIsArticlesChecked(!isArticlesChecked)}>
+                    <div className={`${styles.categoriesItem} ${isArticlesChecked && styles.checkedCategoriesItem}`}>
                         <input type="checkbox" id="articles" checked={isArticlesChecked} hidden/>
-                        <label htmlFor="articles">
+                        <label onClick={() => setIsArticlesChecked(!isArticlesChecked)} htmlFor="articles">
                             {
                                 isArticlesChecked
                                     ? <img src="https://www.svgrepo.com/show/286554/check.svg"
@@ -123,62 +86,117 @@ function Header() {
                             Articles
                         </label>
                     </div>
-                    <div className={styles.categoriesItem}>
+                    <div className={`${styles.categoriesItem} ${isPhotoChecked && styles.checkedCategoriesItem}`}>
                         <input type="checkbox" id="photos" checked={isPhotoChecked} hidden/>
-                        <label htmlFor="photos">Photos</label>
+                        <label onClick={() => setIsPhotoChecked(!isPhotoChecked)} htmlFor="photos">
+                            {
+                                isPhotoChecked
+                                    ? <img src="https://www.svgrepo.com/show/286554/check.svg"
+                                           alt="checked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                                    : <img src="https://www.svgrepo.com/show/311991/cross-mark.svg"
+                                           alt="unchecked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                            }
+                            Photos</label>
                     </div>
-                    <div className={styles.categoriesItem}>
+                    <div className={`${styles.categoriesItem} ${isVideosChecked && styles.checkedCategoriesItem}`}>
                         <input type="checkbox" id="videos" checked={isVideosChecked} hidden/>
-                        <label htmlFor="videos">Videos</label>
-                        return(
-                        <div className='wrapper'>
-                            <div className="header-container">
-                                <div className="box">
-                                    <div className="search-icon">
-                                        <img
-                                            onClick={search}
-                                            src="https://www.svgrepo.com/show/18282/search.svg"
-                                            alt="search"
-                                            width='26px'
-                                            height='26px'
-                                        />
-                                    </div>
-                                    <input value={input}
-                                           onKeyDown={onKeyDownInput}
-                                           onChange={(e) => setInput(e.currentTarget.value)}
-                                           type="text" className='search-input'
-                                           placeholder='Search...'/>
-                                    <div onClick={onClickChevron}
-                                         className={`chevron${activeChevron ? ' chevronReverse' : ''}`}>
-                                        <img src="https://www.svgrepo.com/show/183224/chevron.svg"
-                                             alt="chevron"
-                                             width='26px'
-                                             height='26px'
-                                        />
-                                    </div>
-                                </div>
-                                {transformedQuery}
-
-                                {activeChevron &&
-                                <div className="categories">
-                                    <div className="checkbox-item">
-                                        <input type="checkbox" id='Статьи'/>
-                                        <label htmlFor="Статьи">Статьи</label>
-                                    </div>
-
-                                    <div className="checkbox-item">
-                                        <input type="checkbox" id='Фото'/>
-                                        <label htmlFor="Фото">Фото</label>
-                                    </div>
-
-                                    <div className="checkbox-item">
-                                        <input type="checkbox" id='Видео'/>
-                                        <label htmlFor="Видео">Видео</label>
-                                    </div>
-                                </div>}
-
-                            </div>
-                        </div>
+                        <label onClick={() => setIsVideosChecked(!isVideosChecked)} htmlFor="videos">
+                            {
+                                isVideosChecked
+                                    ? <img src="https://www.svgrepo.com/show/286554/check.svg"
+                                           alt="checked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                                    : <img src="https://www.svgrepo.com/show/311991/cross-mark.svg"
+                                           alt="unchecked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                            }
+                            Videos</label>
+                    </div>
+                    <div className={`${styles.categoriesItem} ${isExcelChecked && styles.checkedCategoriesItem}`}>
+                        <input type="checkbox" id="articles" checked={isExcelChecked} hidden/>
+                        <label onClick={() => setIsExcelChecked(!isExcelChecked)} htmlFor="articles">
+                            {
+                                isExcelChecked
+                                    ? <img src="https://www.svgrepo.com/show/286554/check.svg"
+                                           alt="checked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                                    : <img src="https://www.svgrepo.com/show/311991/cross-mark.svg"
+                                           alt="unchecked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                            }
+                            Excel
+                        </label>
+                    </div>
+                    <div className={`${styles.categoriesItem} ${isPdfChecked && styles.checkedCategoriesItem}`}>
+                        <input type="checkbox" id="articles" checked={isPdfChecked} hidden/>
+                        <label onClick={() => setIsPdfChecked(!isPdfChecked)} htmlFor="articles">
+                            {
+                                isPdfChecked
+                                    ? <img src="https://www.svgrepo.com/show/286554/check.svg"
+                                           alt="checked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                                    : <img src="https://www.svgrepo.com/show/311991/cross-mark.svg"
+                                           alt="unchecked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                            }
+                            Pdf
+                        </label>
+                    </div>
+                    <div className={`${styles.categoriesItem} ${isWordChecked && styles.checkedCategoriesItem}`}>
+                        <input type="checkbox" id="articles" checked={isWordChecked} hidden/>
+                        <label onClick={() => setIsWordChecked(!isWordChecked)} htmlFor="articles">
+                            {
+                                isWordChecked
+                                    ? <img src="https://www.svgrepo.com/show/286554/check.svg"
+                                           alt="checked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                                    : <img src="https://www.svgrepo.com/show/311991/cross-mark.svg"
+                                           alt="unchecked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                            }
+                            Word
+                        </label>
+                    </div>
+                    <div className={`${styles.categoriesItem} ${isPowerpointChecked && styles.checkedCategoriesItem}`}>
+                        <input type="checkbox" id="articles" checked={isPowerpointChecked} hidden/>
+                        <label onClick={() => setIsPowerpointChecked(!isPowerpointChecked)} htmlFor="articles">
+                            {
+                                isPowerpointChecked
+                                    ? <img src="https://www.svgrepo.com/show/286554/check.svg"
+                                           alt="checked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                                    : <img src="https://www.svgrepo.com/show/311991/cross-mark.svg"
+                                           alt="unchecked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                            }
+                            PowerPoint
+                        </label>
                     </div>
                 </div>
             </div>
