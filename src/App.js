2510e94b235af3dcdac3e39d92aacbb54cd24ce3
Nedomorf <danikp01@,ail.ru>
2021-10-02 20:12:41 +0300

    refactor main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import './App.css';
 import Header from "./components/Header";
-import Content from "./components/Content";
 
 
 function App() {
-  return (
-    <div className="App">
-        <div className="wrapper">
-            <Header />
-            <Content />
+    return (
+        <div className="App">
+            <Header/>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,111 @@
-import './Header.css';
-import {useState} from "react";
+import React, {useState} from "react";
+import styles from './Header.module.css';
 
-function Header(){
+function Header() {
     const [activeChevron, setChevron] = useState(false);
+    const [isArticlesChecked, setIsArticlesChecked] = useState(true);
+    const [isPhotoChecked, setIsPhotoChecked] = useState(false);
+    const [isVideosChecked, setIsVideosChecked] = useState(true);
 
-    function onClickChevron(){
+    function onClickChevron() {
         setChevron(!activeChevron);
     }
 
-    return(
-        <div className='wrapper'>
-            <div className="header-container">
-                <div className="box">
-                    <div className="search-icon">
-                        <img src="https://www.svgrepo.com/show/18282/search.svg"
-                             alt="search"
-                             width='26px'
-                             height='26px'
-                        />
+    return (
+        // <div className='main'>
+        //     {/*<div className="header-container">*/}
+        //     <div className="search">
+        //         <div className="search-icon">
+        //             <img src="https://www.svgrepo.com/show/18282/search.svg"
+        //                  alt="search"
+        //                  width='26px'
+        //                  height='26px'
+        //             />
+        //         </div>
+        //         <input type="text" className='search-input' placeholder='Search...'/>
+        //         <div onClick={onClickChevron} className={`'chevron' ${!activeChevron && 'chevronReverse'}`}>
+        //             <img src="https://www.svgrepo.com/show/183224/chevron.svg"
+        //                  alt="chevron"
+        //                  width='26px'
+        //                  height='26px'
+        //             />
+        //         </div>
+        //     </div>
+        //
+        //     {activeChevron &&
+        //     <div className="categories">
+        //         <div className="checkbox-item">
+        //             <input type="checkbox" id="articles"/>
+        //             <label htmlFor="articles">Статьи</label>
+        //         </div>
+        //
+        //         <div className="checkbox-item">
+        //             <input type="checkbox" id="photo"/>
+        //             <label htmlFor="photo">Фото</label>
+        //         </div>
+        //
+        //         <div className="checkbox-item">
+        //             <input type="checkbox" id="videos"/>
+        //             <label htmlFor="videos">Видео</label>
+        //         </div>
+        //     </div>
+        //     }
+        //     {/*</div>*/}
+        // </div>
+        <div className={styles.main}>
+            <div className={styles.search}>
+                <div className={styles.searchIcon}>
+                    <img src="https://www.svgrepo.com/show/18282/search.svg"
+                         alt="search"
+                         width='26px'
+                         height='26px'
+                    />
+                </div>
+                <input type="text" className={styles.searchInput} placeholder='Search...'/>
+                {/*(!activeChevron && styles.chevronReverse)*/}
+                <div onClick={() => setChevron(!activeChevron)}
+                     className={`${styles.chevron} ${!activeChevron && styles.chevronReverse}`}>
+                    <img src="https://www.svgrepo.com/show/183224/chevron.svg"
+                         alt="chevron"
+                         width='26px'
+                         height='26px'
+                    />
+                </div>
+            </div>
+            {/*// TODO*/}
+            {/*// calculate height depends on items num ( (itemHeight * i) / 4 )*/}
+            <div className={styles.categoriesContainer}>
+                <div className={`${styles.categories} ${activeChevron && styles.activeCategories}`}>
+                    <div className={styles.categoriesHeader}></div>
+                    <div className={`${styles.categoriesItem} ${isArticlesChecked && styles.checkedCategoriesItem}`}
+                         onClick={() => setIsArticlesChecked(!isArticlesChecked)}>
+                        <input type="checkbox" id="articles" checked={isArticlesChecked} hidden/>
+                        <label htmlFor="articles">
+                            {
+                                isArticlesChecked
+                                    ? <img src="https://www.svgrepo.com/show/286554/check.svg"
+                                           alt="checked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                                    : <img src="https://www.svgrepo.com/show/311991/cross-mark.svg"
+                                           alt="unchecked"
+                                           width='26px'
+                                           height='26px'
+                                    />
+                            }
+                            Articles
+                        </label>
                     </div>
-                    <input type="text" className='search-input' placeholder='Search...'/>
-                    <div onClick={ onClickChevron } className={`chevron${activeChevron?' chevronReverse':''}`}>
-                        <img src="https://www.svgrepo.com/show/183224/chevron.svg"
-                             alt="chevron"
-                             width='26px'
-                             height='26px'
-                        />
+                    <div className={styles.categoriesItem}>
+                        <input type="checkbox" id="photos" checked={isPhotoChecked} hidden/>
+                        <label htmlFor="photos">Photos</label>
+                    </div>
+                    <div className={styles.categoriesItem}>
+                        <input type="checkbox" id="videos" checked={isVideosChecked} hidden/>
+                        <label htmlFor="videos">Videos</label>
                     </div>
                 </div>
-
-                {activeChevron &&
-                    <div className="categories">
-                        <div className="checkbox-item">
-                            <input type="checkbox" id='Статьи'/>
-                            <label htmlFor="Статьи">Статьи</label>
-                        </div>
-
-                        <div className="checkbox-item">
-                            <input type="checkbox" id='Фото'/>
-                            <label htmlFor="Фото">Фото</label>
-                        </div>
-
-                        <div className="checkbox-item">
-                            <input type="checkbox" id='Видео'/>
-                            <label htmlFor="Видео">Видео</label>
-                        </div>
-                    </div>
-                }
             </div>
         </div>
     )
